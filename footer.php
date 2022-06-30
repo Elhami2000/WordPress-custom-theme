e0563d7ceffbcc29bc40987278918dcce7d019d4
Elhami2000 <em49455@ubt-uni.net>
2022-06-30 21:27:06 +0200

    Added some stuff

---
+++ footer.php
@@ -0,0 +1,15 @@
+<footer class="footer text-center py-2 theme-bg-dark">
+		   
+            <p class="copyright"><a href="https://www.youtube.com/channel/UClF9wX24Z7MM2cYZBpnHYQQ">Elhami Musliu</a></p>
+		   
+	    </footer>
+    
+    </div>
+
+<?php 
+     wp_footer();
+?>
+    
+</body>
+</html> 
+
