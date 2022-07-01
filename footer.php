8af0aafba8da0afb820b82b5789af2f237b67dc7
Elhami2000 <em49455@ubt-uni.net>
2022-07-01 20:09:04 +0200

    Finished developing the new theme

--- footer.php
+++ footer.php
@@ -1,15 +1,13 @@
 <footer class="footer text-center py-2 theme-bg-dark">
-		   
-            <p class="copyright"><a href="https://www.youtube.com/channel/UClF9wX24Z7MM2cYZBpnHYQQ">Elhami Musliu</a></p>
-		   
-	    </footer>
-    
-    </div>
 
-<?php 
-     wp_footer();
-?>
-    
+    <?php dynamic_sidebar('footer-1'); ?>
+
+</footer>
+
+</div>
+
+<?php wp_footer(); ?>
+
 </body>
-</html> 
 
+</html>
